a3168c0d7489f26f9622679d1b47f6931653cd84
Caleb Kaiser <caleb@cobrakai.edu>
2022-10-13 11:16:17 -0400

    Apply CSS injection priority + update .gitignore

--- frontend/components/page.client.js
+++ frontend/components/page.client.js
@@ -1,10 +1,11 @@
+import { StyledEngineProvider } from '@mui/material';
 
 export default function Page({ children }) {
     return (
-        <>
+        <StyledEngineProvider injectFirst>
             <div className="main">
                 <div className="page">{children}</div>
             </div>
-        </>
+        </StyledEngineProvider>
     );
 }
