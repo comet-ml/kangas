31b78eb597a25656f84cc41213bc77b27f108038
Douglas Blank <doug.blank@gmail.com>
2022-10-14 00:02:44 -0700

    JSON is now returned decoded from string

--- frontend/components/Cells/JSONCell.server.js
+++ frontend/components/Cells/JSONCell.server.js
@@ -1,15 +1,10 @@
 const JSONCell = ({ value }) => {
-    let jsonValue;
-    try {
-        jsonValue = JSON.parse(value);
-    } catch {
-        jsonValue = { ERROR: `expecting JSON but got '${value}'` };
-    }
+    const jsonValue = value;
 
-    const strValue = Object.keys(jsonValue)
+    const strValue = "(" + Object.keys(jsonValue)
         .filter((key) => ['string', 'number', 'boolean'].includes(typeof key))
-        .map((key) => `${key}: ${jsonValue[key]}`)
-        .join(', ');
+        .map((key) => `"${key}": ${jsonValue[key]}`)
+        .join(', ') + "}";
     return <div className="cell-content json">{strValue}</div>;
 };
 
