12a9f94ea4ec8bcb7d2a2c95123178968ee8329f
Caleb Kaiser <caleb@cobrakai.edu>
2022-10-14 10:48:40 -0400

    readd import

--- frontend/components/page.client.js
+++ frontend/components/page.client.js
@@ -1,12 +1,11 @@
 import { StyledEngineProvider } from "@mui/material"
-
 const Page = ({ children }) => {
     return (
-	    <>
+        <StyledEngineProvider injectFirst>
             <div className="main">
                     <div className="page">{children}</div>        
             </div>
-        </>
+        </StyledEngineProvider>
     );
 }
 
