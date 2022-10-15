a8dcb555d2febd16d16747f87d860db8b1b05502
Doug Blank <doug.blank@gmail.com>
2022-10-15 09:41:24 -0700

    Test all dataframe types

---
+++ backend/tests/unit/test_dataframe.py
@@ -0,0 +1,53 @@
+import kangas
+from pandas.util.testing import (
+    makeCustomDataframe,
+    makeDataFrame,
+    makeMissingDataframe,
+    makeMixedDataFrame,
+    makePeriodFrame,
+    makeTimeDataFrame,
+)
+
+
+def test_makeCustomDataframe():
+    df = makeCustomDataframe(100, 25)
+    dg = kangas.read_dataframe(df)
+    assert len(dg) == 100
+    assert len(dg[0]) == 25
+    dg.save()
+    
+def test_makeDataFrame():
+    df = makeDataFrame()
+    dg = kangas.read_dataframe(df)
+    assert len(dg) == 30
+    assert len(dg[0]) == 4
+    dg.save()
+
+def test_makeMissingDataframe():
+    df = makeMissingDataframe()
+    dg = kangas.read_dataframe(df)
+    assert len(dg) == 30
+    assert len(dg[0]) == 4
+    dg.save()
+    
+def test_makeMixedDataFrame():
+    df = makeMixedDataFrame()
+    dg = kangas.read_dataframe(df)
+    assert len(dg) == 5
+    assert len(dg[0]) == 4
+    dg.save()
+    
+def test_makePeriodFrame():
+    df = makePeriodFrame()
+    dg = kangas.read_dataframe(df)
+    assert len(dg) == 30
+    assert len(dg[0]) == 4
+    dg.save()
+    
+def test_makeTimeDataFrame():
+    df = makeTimeDataFrame()
+    dg = kangas.read_dataframe(df)
+    assert len(dg) == 30
+    assert len(dg[0]) == 4
+    dg.save()
+    
