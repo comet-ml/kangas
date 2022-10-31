28a80999d2a06f289881b53b16e9c063ca12422a
Caleb Kaiser <caleb@cobrakai.edu>
2022-10-31 12:40:46 -0400

    Make react-autocomplete-input work

--- frontend/components/SettingsBar/FilterExpr.client.js
+++ frontend/components/SettingsBar/FilterExpr.client.js
@@ -1,17 +1,12 @@
 import { unstable_useRefreshRoot as useRefreshRoot } from 'next/streaming';
 //import TextField from '@mui/material/TextField';
-import TextField from 'react-autocomplete-input';
+import Autocomplete from './ReactAutocomplete';
+import { TextField } from '@mui/material';
 import InputAdornment from '@mui/material/InputAdornment';
 import { useCallback, useEffect } from 'react';
 import DialogueModal from '../Modals/DialogueModalContainer.client';
 
 import HelpText from './HelpText.js';
-
-const FilterButton = ({ callback }) => (
-    <div className="button-outline" onClick={callback}>
-        <img src="/filter_placeholder.png" /> <span>Filter</span>
-    </div>
-);
 
 const HelpButton = () => (
     <div className="button-outline">
@@ -57,27 +52,20 @@ const FilterExpr = ({ query, columns }) => {
 
     return (
         <>
-            <TextField
-                trigger={["{"]} options={{"{": columns.map(name => `"${name}"`)}}
-	        changeOnSelect={(trigger, slug) => `{${slug}}`}
-	        matchAny={true}
+            <Autocomplete
+                trigger={["{"]} 
+                options={{"{": columns.map(name => `"${name}"`)}}
+                changeOnSelect={(trigger, slug) => `{${slug}}`}
+                matchAny={true}
                 regex={'^[a-zA-Z0-9_\\-\\"\\ ]+$'}
-                Component={'textarea'}
-	        spacer={' '}
-	        maxOptions={0}
-	        spaceRemovers={['.']}
+                Component='textarea'
+                spacer={' '}
+                maxOptions={0}
+                spaceRemovers={['.']}
                 placeholder={`e.g.: {"column name"} > 0.5`}
                 id="filter"
-                sx={{
-                    width: '360px'
-                }}
-                InputProps={{
-                    endAdornment: <FilterButton callback={applyFilter} />,
-                    sx: {
-                        fontSize: '13px'
-                    }
-                }}
                 onKeyPress={onKeyPress}
+                applyFilter={applyFilter}
             />
             <DialogueModal fullScreen={false} toggleElement={<HelpButton />}>
                 <HelpText />
