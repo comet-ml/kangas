057c2226c9a0fbb61578e14e68ebabfaa99be10a
Caleb Kaiser <caleb@cobrakai.edu>
2022-11-03 11:29:11 -0400

    remove code in _app

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,9 +1,7 @@
 import '../components/page.css';
 import '../components/Cells/Image/ImageCanvas.css';
 import '../components/SettingsBar/HelpText.css';
-import config from '../config';
 import Head from 'next/head';
-import Script from 'next/script';
 
 const MyApp = ({ Component, pageProps }) => {
     return (
@@ -11,7 +9,6 @@ const MyApp = ({ Component, pageProps }) => {
             <Head>
                 <link rel="icon" type="image/png" href="/favicon.png" />
                 <title>Kangas - Data and Model Analysis</title>
-                { config?.scripts?.map(url => <Script src={url}/>) }
             </Head>
             <Component {...pageProps} />
         </>
