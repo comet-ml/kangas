1f7eaf81985cca530527941f8a21383bcbfeb5a8
Caleb Kaiser <caleb@cobrakai.edu>
2022-11-03 11:26:21 -0400

    cleanup consolelogs etc.

--- frontend/config.js
+++ frontend/config.js
@@ -1,15 +1,5 @@
 import {env} from 'process';
 
-const parseScripts = (scripts) => {
-    console.log('hey');
-    console.log(scripts)
-    try {
-        return JSON.parse(`[ ${scripts} ]`)
-    } catch (error) {
-        console.error(error)
-        return []
-    }
-}
 const localConfig = {
     apiUrl: `${env.KANGAS_PROTOCOL || 'http'}://${env.KANGAS_HOST}:${env.KANGAS_BACKEND_PORT}/datagrid/`,
     defaultDecimalPrecision: 5,
