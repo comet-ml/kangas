54cdd254fdd4b191ff69dd1bc831585298ee51d5
Douglas Blank <doug.blank@gmail.com>
2022-11-22 11:18:19 -0800

    Allow JSON lists in the JSONCell

--- frontend/components/Cells/JSONCell.server.js
+++ frontend/components/Cells/JSONCell.server.js
@@ -1,11 +1,7 @@
 const JSONCell = ({ value }) => {
-    const jsonValue = value;
+    const jsonValue = value ? JSON.stringify(value) : "None";
 
-    const strValue = "{" + Object.keys(jsonValue)
-        .filter((key) => ['string', 'number', 'boolean'].includes(typeof key))
-        .map((key) => `"${key}": ${jsonValue[key]}`)
-        .join(', ') + "}";
-    return <div className="cell-content json">{strValue}</div>;
+    return <div className="cell-content json">{jsonValue}</div>;
 };
 
 export default JSONCell;
