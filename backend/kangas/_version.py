5984226ae4c9e9e25c54e0243cca0316379a9c41
Douglas Blank <doug.blank@gmail.com>
2022-12-05 08:16:04 -0800

    Bump version to 1.3.1

--- backend/kangas/_version.py
+++ backend/kangas/_version.py
@@ -11,5 +11,5 @@
 #    All rights reserved                             #
 ######################################################
 
-version_info = (1, 3, 0)
+version_info = (1, 3, 1)
 __version__ = ".".join(map(str, version_info))
