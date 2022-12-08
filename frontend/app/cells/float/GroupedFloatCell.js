ea960ee7366693ef4c0193da3c1b802c9e2ddf35
Douglas Blank <doug.blank@gmail.com>
2022-12-08 10:26:46 -0800

    Typo in case of filename
    Case sensitive on Linux :)

--- frontend/app/cells/float/GroupedFloatCell.js
+++ frontend/app/cells/float/GroupedFloatCell.js
@@ -1,4 +1,4 @@
-import Histogram from "../charts/Histogram/Histogram";
+import Histogram from "../charts/histogram/Histogram";
 import isPrimitive from '../../../lib/isPrimitive';
 import formatValue from "../../../lib/formatValue";
 import classNames from 'classnames/bind';
