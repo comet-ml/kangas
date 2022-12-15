4fdb032f92447bc8dbf0c8ee68a1ed3aeaa5568b
Douglas Blank <doug.blank@gmail.com>
2022-12-15 11:23:49 -0800

    Added CSS for BaseImageCell

--- frontend/app/cells/image/BaseImageCell.js
+++ frontend/app/cells/image/BaseImageCell.js
@@ -3,13 +3,19 @@ import ImageCanvasCell from './ImageCanvasCell';
 import { Suspense } from 'react';
 // TODO create a parseDataURL helper
 
+import classNames from 'classnames/bind';
+import styles from '../Cell.module.scss';
+
+const cx = classNames.bind(styles);
+
+
 const PlainImageCell = async ({ value, query, expanded=false }) => {
     const { type, assetType, assetId } = value;
     const { dgid } = query;
     const image = await fetchAsset({ query: { assetId, dgid, thumbnail: true }, returnUrl: true });
 
     return (
-            <div className="cell-content">
+            <div className={cx("cell-content")}>
                 <img
                     src={`data:application/octet-stream;base64,${image}`}
                     alt="DataGrid Image"
