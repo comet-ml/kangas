83fd6e44e36e77ab27e84ab31e95b8579edcc8c0
Caleb Kaiser <caleb@cobrakai.edu>
2022-12-28 14:22:50 -0500

    improved resizing/fixed bug with multiple resizes

--- frontend/app/cells/baseClient.js
+++ frontend/app/cells/baseClient.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useContext, useCallback } from "react";
+import { useContext, useCallback, useState, useEffect } from "react";
 import { ViewContext } from "../contexts/ViewContext";
 import classNames from 'classnames/bind';
 import styles from './Cell.module.scss';
@@ -9,15 +9,36 @@ import defaultCellSizes from "../../lib/consts/defaultCellSizes";
 
 const cx = classNames.bind(styles);
 
+
+/* 
+The startResizing/stopResizing/initWidth dynamic is potentially a bit confusing.
+The gist is that we need to update the width of every cell in a column as we resize
+a given cell. Because of the way react-resizable works under the hood, simply updating
+the cell widths via onResize will often lead to the widths of different cells falling out of sync.
+Using the startResizing/stopResizing/initWidth dynamic, we can force all widths to stay in sync.
+*/
+ 
 const CellClient = ({ columnName, type, isHeader, children }) => {
     const { columns, updateWidth } = useContext(ViewContext);
+    const [isResizing, setIsResizing] = useState(false);
+    const [initWidth, setInitWidth] = useState(defaultCellSizes[type].width)
+
     const resize = useCallback((d) => {
         updateWidth({
             [columnName]: {
-                width: defaultCellSizes[type].width + d.width
+                width: initWidth + d.width
             }
         })
-    }, [columnName, columns]);
+    }, [columnName, initWidth]);
+
+    const startResizing = useCallback(() => setIsResizing(true), []);
+    const stopResizing = useCallback(() => setIsResizing(false), []);
+
+    useEffect(() => {
+        if (!isResizing && (columns?.[columnName]?.width !== initWidth)) {
+            setInitWidth(columns?.[columnName]?.width)
+        }
+    }, [isResizing, columns?.[columnName]?.width, initWidth])
 
     return (
         <Resizable 
@@ -29,9 +50,11 @@ const CellClient = ({ columnName, type, isHeader, children }) => {
             enable={{
                 right: true
             }}
+            onResizeStart={startResizing}
             onResize={(e, direction, ref, d) => {
                 resize(d)
-            }}            
+            }}
+            onResizeStop={stopResizing}
         >
             { children }
         </Resizable>
