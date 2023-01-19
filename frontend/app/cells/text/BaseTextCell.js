9f4cda7eeeaf5b962bdf48fc60ca9fa211b41c25
Douglas Blank <doug.blank@gmail.com>
2023-01-19 15:16:34 -0800

    Revert "Linkify text"

--- frontend/app/cells/text/BaseTextCell.js
+++ frontend/app/cells/text/BaseTextCell.js
@@ -1,16 +1,11 @@
-import linkify from "linkify-string";
+import formatValue from "../../../lib/formatValue"
 
-import formatValue from "../../../lib/formatValue";
-
-const TextCell = ({ value, style, expanded=false }) => {
-
-    const text = expanded ? linkify(formatValue(value, 'TEXT')) : formatValue(value, 'TEXT');
-
+const TextCell = ({ value, style }) => {
     return (
         <div className="cell-content" style={style}>
-            <div dangerouslySetInnerHTML={{__html: text}} />
+            {`${formatValue(value, 'TEXT')}`}
         </div>
-    );
+    )
 
 }
 
