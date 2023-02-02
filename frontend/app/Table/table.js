6cfba4ff82e4186ea91f485fc6647fcd2f5255e8
Douglas Blank <doug.blank@gmail.com>
2023-02-02 13:51:09 -0800

    Added flag to switch back and forth between transposed table

--- frontend/app/Table/table.js
+++ frontend/app/Table/table.js
@@ -6,56 +6,73 @@ import classNames from 'classnames/bind';
 import { Suspense } from 'react';
 import Skeleton from '../Skeleton';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 const transpose = (matrix) => {
-    return matrix.reduce((prev, next) => Object.values(next).map((item, i) =>
-      (prev[i] || []).concat(Object.values(next)[i])
+    return matrix.reduce((prev, next) => Object.values(next).map(
+        (item, i) =>
+            (prev[i] || []).concat(Object.values(next)[i])
     ), []);
-  }
+};
 
-  export const TableDisplay = ({ query, data }) => {
+export const TableDisplay = ({ query, data }) => {
     const { columnTypes, columns, rows, displayColumns } = data;
+    const transposeTable = false;
 
     // Remove any row keys that are not in displayColumns:
     const displayRows = rows.map(row => Object.fromEntries(
-	    Object.entries(row).filter(([name]) => displayColumns.includes(name))
+	Object.entries(row).filter(([name]) => displayColumns.includes(name))
     ));
 
-    const transposed = transpose([ displayColumns, ...displayRows ]);
 
-    return (
-        <div className={styles.tableRoot}>
-            {transposed?.map((column, colidx) => (
-                <div className={cx('column')} key={`col-${colidx}`}>
+    if (transposeTable) {
+        const transposed = transpose([ displayColumns, ...displayRows ]);
+
+        return (
+                <div className={styles.tableRoot} style={{display: 'flex'}}>
+                {transposed?.map((column, colidx) => (
+                        <div className={cx('column')} key={`col-${colidx}`}>
+                        {
+                            Object.values(column).map( (cell, cidx) => (
+                                    <Suspense fallback={<Skeleton message={`suspending ${cell} - ${colidx}/${cidx}`} />}>
+                                    <Cell
+                                value={cell}
+                                type={columnTypes[colidx]}
+                                columnName={columns[colidx]}
+                                query={query}
+                                isHeader={cidx < 1}
+                                    />
+                                    </Suspense>
+                        ) )
+                    }
+                    </div>
+                ))}
+                </div>
+        );
+    } else {
+
+      return (
+        <div className={styles.tableRoot} style={{display: 'unset'}}>
+            {[ displayColumns, ...displayRows ]?.map((row, ridx) => (
+                <div className={cx('row', { group: !!query?.groupBy, headerRow: ridx < 1 })} key={`row-${ridx}`}>
                     {
-                        Object.values(column).map( (cell, cidx) => (
-                            <Suspense fallback={<Skeleton message={`suspending ${cell} - ${colidx}/${cidx}`} />}>
-                                <Cell
-                                    value={cell}
-                                    type={columnTypes[colidx]}
-                                    columnName={columns[colidx]}
-                                    query={query}
-                                    isHeader={cidx < 1}
-                                />
-                            </Suspense>
+                        Object.values(row).map( (cell, cidx) => (
+                          <Suspense fallback={<Skeleton message={`suspending ${cell} - ${cidx}`} />}>
+                            <Cell
+                                value={cell}
+                                type={columnTypes[cidx]}
+                                columnName={columns[cidx]}
+                                query={query}
+                                isHeader={ridx < 1}
+                            />
+                          </Suspense>
                         ) )
                     }
                 </div>
                 ))}
         </div>
-    );
-
+     );
+   }
 };
-/*
-const Table = async ({ query }) => {
-    const data = await fetchDataGrid(query);
 
-    return (
-        <Suspense fallback={<TableDisplay data={EMPTY} query={query} />}>
-            <TableDisplay data={data} query={query} />
-        </Suspense>
-    );
-};
- */
 export default TableDisplay;
