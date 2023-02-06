c7cb9ea219847d91fc9c467eb17a83a70879baf5
Douglas Blank <doug.blank@gmail.com>
2023-02-06 11:42:42 -0800

    Added BooleanCell type

--- frontend/app/cells/boolean/BaseBooleanCell.js
+++ frontend/app/cells/boolean/BaseBooleanCell.js
@@ -4,7 +4,6 @@ import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 
 const BooleanCell = ({ value, style }) => {
-    console.log("BaseBooleanCell!");
     return (
         <div className="cell-content" style={style}>
             {
