1d37ac902c1bea4e1fd202d12d6e1d46db810819
Douglas Blank <doug.blank@gmail.com>
2023-03-13 10:05:32 -0700

    Verison 2.0.0-beta7, added waitress

--- backend/kangas/_version.py
+++ backend/kangas/_version.py
@@ -11,5 +11,5 @@
 #    All rights reserved                             #
 ######################################################
 
-version_info = (2, 0, "0beta6")
+version_info = (2, 0, "0beta7")
 __version__ = ".".join(map(str, version_info))
