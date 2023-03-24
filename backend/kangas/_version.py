70e23f7b6c382fd52cabfce80c49c983ccd545a9
Douglas Blank <doug.blank@gmail.com>
2023-03-24 14:21:16 -0700

    Version 2.2.0

--- backend/kangas/_version.py
+++ backend/kangas/_version.py
@@ -11,5 +11,5 @@
 #    All rights reserved                             #
 ######################################################
 
-version_info = (2, 1, 1)
+version_info = (2, 2, 0)
 __version__ = ".".join(map(str, version_info))
