1e642d71ee4bbabf60841e05d271571608e52b92
Doug Blank <doug.blank@gmail.com>
2023-04-09 18:25:50 -0700

    Dynamic updates

--- frontend/app/Settings/RefreshButton.js
+++ frontend/app/Settings/RefreshButton.js
@@ -1,21 +1,46 @@
 'use client';
 
 import CachedIcon from '@mui/icons-material/Cached';
-import { useCallback, useContext } from 'react';
+import { useCallback, useContext, useEffect } from 'react';
 import useQueryParams from '../../lib/hooks/useQueryParams';
 
 import styles from './RefreshButton.module.scss';
 import classNames from 'classnames/bind';
 import { ViewContext } from '../contexts/ViewContext';
+import { ConfigContext } from '../contexts/ConfigContext';
 const cx = classNames.bind(styles);
+
 
 const RefreshButton = ({ query }) => {
     const { params, updateParams } = useQueryParams();
     const { columns, view } = useContext(ViewContext);
+    const { config } = useContext(ConfigContext);
 
+    const checkForUpdates = useCallback(async () => {
+        if (!!query?.timestamp) {
+            try {
+                fetch(`${config.rootUrl}api/timestamp?dgid=${query?.dgid}`).then(async res => {
+                    const currentTimestamp = (await res.json()).timestamp;
+                    if (!!currentTimestamp) {
+                        if (query.timestamp !== currentTimestamp)
+                            updateParams({...params});
+                    }
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }, [query?.timestamp, query?.dgid, params, updateParams]);
+
+    useEffect(async () => {
+        const interval = setInterval(async () => {
+            await checkForUpdates();
+        }, 5000);
+        return () => clearInterval(interval);
+    }, []);
 
     const clearCache = useCallback(() => {
-	// Reset params:
+        // Reset params:
         updateParams({
             sort: undefined,
             group: undefined,
