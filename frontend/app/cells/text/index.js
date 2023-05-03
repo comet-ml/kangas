e232ab9753b501ca3e577abb162795757b7de2b0
Douglas Blank <doug.blank@gmail.com>
2023-05-03 10:33:01 -0700

    Added category colors to text categories; always compute stats with 'kangas upgrade'

--- frontend/app/cells/text/index.js
+++ frontend/app/cells/text/index.js
@@ -1,10 +1,16 @@
 import Base from './BaseTextCell';
 import Grouped from './GroupedTextCell';
+import fetchMetadata from '@kangas/lib/fetchMetadata';
 import DialogueModal from '@kangas/app/modals/DialogueModal/DialogueModalClient';
 
-const TextCell = ({ value, query, style, ssr }) => {
+const TextCell = async ({ value, query, style, ssr, columnName }) => {
+    const metadata = await fetchMetadata({
+        query: {dgid: query?.dgid, timestamp: query?.timestamp},
+        ssr: true,
+    });
+
     if (!query?.groupBy) return (
-        <DialogueModal toggleElement={<Base value={value} query={query}  style={style} />}><Base value={value} query={query} /></DialogueModal>
+        <DialogueModal toggleElement={<Base value={value} query={query} style={style} metadata={metadata?.[columnName]} />}><Base value={value} query={query} style={style} metadata={metadata?.[columnName]} /></DialogueModal>
     );
     else  return (
         <DialogueModal toggleElement={<Grouped value={value} query={query} ssr={ssr} />}><Grouped value={value} query={query} expanded={true} ssr={ssr} /></DialogueModal>
