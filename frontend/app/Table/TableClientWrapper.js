ffc987ea70580c43d64d12b9e4da83c702f663cf
Caleb Kaiser <caleb@cobrakai.edu>
2023-05-17 09:53:35 -0400

    center spinner

--- frontend/app/Table/TableClientWrapper.js
+++ frontend/app/Table/TableClientWrapper.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { CircularProgress } from "@mui/material";
+import { CircularProgress, Dialog } from "@mui/material";
 import { useContext, useEffect, useRef } from "react"
 import { ViewContext } from "@kangas/app/contexts/ViewContext"
 
@@ -15,29 +15,21 @@ const TableClientWrapper = ({ data, children }) => {
         }
     }, [data, completeLoading, isLoading]);
 
-    if (isLoading) {
         return (
-            <div>
-                <div
-                    style={{
-                        position: 'absolute',
-                        width: '100%',
-                        height: '100%',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}
+            <>
+                <Dialog open={isLoading}
+                  PaperProps={{
+                    style: {
+                      backgroundColor: 'transparent',
+                      boxShadow: 'none',
+                    },
+                  }}
                 >
-                    <div >
                         <CircularProgress />
-                    </div>
-                </div>
+                </Dialog>
                 { children }
-            </div>
+            </>
         )
-    } else {
-        return <>{ children }</>
-    }
 }
 
 export default TableClientWrapper;
