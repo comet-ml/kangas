c40f635809200d5723deac3f72b986f88102f623
Douglas Blank <doug@comet.com>
2024-11-25 09:48:05 -0500

    Version 2.4.10

--- backend/kangas/_version.py
+++ backend/kangas/_version.py
@@ -11,5 +11,5 @@
 #    All rights reserved                             #
 ######################################################
 
-version_info = (2, 4, 9)
+version_info = (2, 4, 10)
 __version__ = ".".join(map(str, version_info))
